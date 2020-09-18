84ecc3cca13c54dd85a99ac7b0f2f2b12d3b1017
unknown <stowerslab@gmail.com>
2020-09-17 19:23:23 -0700

    remove cell counter dir

--- cellCounter_Pete/temp.m
+++
@@ -1,8 +0,0 @@
-clear all; close all;
-
-matName = 'C:\data\Jason\microscope\2016_01_CrhTdtwithEsrImmuno\male\testBar.mat';
-load(matName);
-[thresh, IbwNissl, nisslCC] = sectionSegmentCountRoi(IsubNissl);
-
-nisslCC.NumObjects
-
